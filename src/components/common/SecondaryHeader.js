259a2a6453ad6c2496fbad0728b7b727dd17dcc5
Nadav Galili <67638018+nadav-galili@users.noreply.github.com>
2023-02-04 16:35:11 +0200

    added secondary comp

---
+++ src/components/common/SecondaryHeader.js
@@ -0,0 +1,17 @@
+import { View, Text, StyleSheet } from "react-native";
+
+const SecondaryHeader = ({ text, color, size }) => {
+    return (
+        <View style={styles.container}>
+            <Text style={{ color, fontSize: size, textDecorationLine: "underline" }}>{text}</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: "center",
+        justifyContent: "center",
+    },
+});
+export default SecondaryHeader;
--- src/screens/MyLeaguesScreen.js
+++ src/screens/MyLeaguesScreen.js
@@ -1,13 +1,16 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 
+import colors from "../../assets/config/colors";
 import LeagueImage from "../components/common/LeagueImage";
 import MyLeaguesPlayerItem from "../components/MyLeaguesPlayerItem";
+import SecondaryHeader from "../components/common/SecondaryHeader";
 import players from "../../assets/data/players";
 import league from "../../assets/data/league";
 const MyLeaguesScreen = () => {
     return (
         <View style={{ marginTop: 40 }}>
             <LeagueImage uri={league.url} width={80} height={80} />
+            <SecondaryHeader text={league.name} color={colors.primaryTextColor} size={20} />
             <View style={styles.container}>
                 {players.map((player) => (
                     <MyLeaguesPlayerItem player={player} key={player.id} />
